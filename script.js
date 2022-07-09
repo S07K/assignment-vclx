03ff4e6442cb2f8b322a49502df99cb6aaa28170
shubham.k <shubham.k@VCX-LP-02.VCLOUDX.LAN>
2022-07-09 20:40:34 +0530

    changed media queries.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var chatBox = document.getElementsByClassName('chat-box');
 chats[0].addEventListener('click', function() {
     if(chatBox[0].classList.contains('active'))  {
         chatBox[0].classList.remove('active');
-        console.log('hey')
     }else {
         chatBox[0].classList.add('active');
     }
